333ad23b68b9c3b400f2b4f05c5533a69dceabc1
akinari-tsuji <akinari.tsuji@bizreach.co.jp>
2025-09-09 14:09:12 +0900

    refactor: 共通処理を抜き出す

--- spec/lib/closed_interval_spec.rb
+++ spec/lib/closed_interval_spec.rb
@@ -1,9 +1,14 @@
 RSpec.describe ClosedInterval do
   describe "閉区間に含むか判定する関数" do
     context "正常系" do
+      let(:closed_interval) { ClosedInterval.new() }
+
       it "[3, 7]の区間の時に、5を渡したらTrueを返すこと" do
-        closed_interval = ClosedInterval.new()
         expect(closed_interval.include?(5)).to eq true
+      end
+
+      it "[3, 7]の区間の時に、7を渡したらTrueを返すこと" do
+        expect(closed_interval.include?(7)).to eq true
       end
     end
   end
