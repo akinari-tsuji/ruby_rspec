5f75e2c26707a0c093de5f3dc8bc167fb2a9a3bd
akinari-tsuji <akinari.tsuji@bizreach.co.jp>
2025-09-09 14:54:10 +0900

    feat: 他の区間にも対応できるように修正（三角測量）

--- lib/closed_interval.rb
+++ lib/closed_interval.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class ClosedInterval
+  attr :lower_endpoint, :upper_endpoint
+
+  def initialize(lower_endpoint, upper_endpoint)
+    @lower_endpoint = lower_endpoint
+    @upper_endpoint = upper_endpoint
+  end
+
   def include?(number)
-    return true if 3 <= number and number <= 7
+    return true if lower_endpoint <= number and number <= upper_endpoint
     false
   end
 end
--- spec/lib/closed_interval_spec.rb
+++ spec/lib/closed_interval_spec.rb
@@ -1,24 +1,26 @@
 RSpec.describe ClosedInterval do
-  let(:closed_interval) { ClosedInterval.new() }
-
   describe "閉区間に含むか判定する関数" do
     context "正常系" do
       context "[3, 7]の区間の時に" do
         it "5を渡したらTrueを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(5)).to eq true
         end
 
         it "7を渡したらTrueを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(7)).to eq true
         end
 
         it "3を渡したらTrueを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(3)).to eq true
         end
       end
 
       context "[1, 5]の区間の時に" do
         it "2を渡したらTrueを返すこと" do
+          closed_interval = ClosedInterval.new(1, 5)
           expect(closed_interval.include?(2)).to eq true
         end
       end
@@ -27,18 +29,22 @@ RSpec.describe ClosedInterval do
     context "異常系" do
       context "[3, 7]の区間の時に" do
         it "8を渡したらFalseを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(8)).to eq false
         end
 
         it "2を渡したらFalseを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(2)).to eq false
         end
 
         it "10を渡したらFalseを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(10)).to eq false
         end
 
         it "1を渡したらFalseを返すこと" do
+          closed_interval = ClosedInterval.new(3, 7)
           expect(closed_interval.include?(1)).to eq false
         end
       end
